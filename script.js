7ac8e2135862adf574d1a65198976c8f90eddcef
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2022-07-06 22:05:20 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstName = "Sammy";
+let lastName = " Erickson";
+let fullName = (firstName + lastName);
+
+console.log(fullName);
+
+let population = "199417";
+
+console.log(population);
+
+console.log(population * 3);
+
+let javaScriptIsCool = true;
+let colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+
+for (let i=1; i<6;i++){
+    console.log(i);
+}
+
+for (let i=0; i<5; i++){
+    console.log(i);
+}
